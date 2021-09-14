b6e9c44d030acbd67847b8d88bb2a8193c352b3a
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-09-14 10:05:44 +0200

    Début

--- back/manager/manager.php
+++ back/manager/manager.php
@@ -2,6 +2,16 @@
 
 class manager {
 
+//Connexion à la base de données
+  public function connexionBdd($db) {
+    try {
+      $db = new PDO('mysql:host=localhost;dbname=;charset=utf8', 'root', '');
+    }
+    catch(Exception $e) {
+      die('Error' : $e->getMessage());
+    }
+  }
+
   public function connexion() {
 
   }
