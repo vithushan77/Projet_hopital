865c64d48516cc4df864a7dbac9c8218721a1e16
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-09-14 11:11:32 +0200

    Premières requêtes Manager

--- back/manager/manager.php
+++ back/manager/manager.php
@@ -3,7 +3,7 @@
 class manager {
 
 //Connexion à la base de données
-  public function connexionBdd($db) {
+  public function connexionBdd() {
     try {
       $db = new PDO('mysql:host=localhost;dbname=;charset=utf8', 'root', '');
     }
@@ -12,16 +12,59 @@ class manager {
     }
   }
 
-  public function connexion() {
+  public function signIn($signin) {
+    $sql = $this->connexionBdd()->prepare('SELECT * FROM user WHERE mail=:mail AND mdp =:mdp');
+    $sql->execute(array(
+      $signin->getMail(),
+      $signin->getMdp()
+    ));
+    $rslt = $sql->fetch();
 
+    if($rslt) {
+      $user = new user ($rslt);
+      return $user;
+    }
+
+    else {
+      return null;
+    }
   }
 
-  public function inscription() {
+  public function insertUser(User $u) {
+    $sql = $this->connexionBdd()->prepare('SELECT nom, prenom FROM user WHERE nom=:nom AND prenom=:prenom');
+    $sql->execute(array(
+      $u->getNom(),
+      $u->getPrenom()
+    ));
+    $rslt = $sql->fetch();
 
+    if($rslt == false) {
+      $sql = $this->connexionBdd()->prepare("INSERT INTO ...(nom, prenom, mail, mdp, role)
+      VALUES(:nom, :prenom, :mail, :mdp, :role)");
+      $sql->execute(array(
+        'nom'=>$u->getNom(),
+        'prenom'=>$u->getPrenom(),
+        'mail'=>$u->getMail(),
+        'mdp'=>$u->getMdp(),
+        'role'=>$u->getRole()
+    ));
+      return 1;
+    }
+    else{
+      return 0;
+    }
   }
 
-  public function modifierProfil() {
-
+  public function modifierProfil(User $u) {
+    $sql = $this->connexionBdd()->prepare('UPDATE ... SET :nom, :prenom, :mail, :mdp, :role');
+    $sql->execute(array(
+      'id'=>$u->getId(),
+      'nom'=>$u->getNom(),
+      'prenom'=>$u->getPrenom(),
+      'mail'=>$u->getMail(),
+      'mdp'=>$u->getMdp(),
+      'role'=>$u->getRole()
+    ));
   }
 }
 
