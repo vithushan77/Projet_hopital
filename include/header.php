6e7425874f9cf3f7d394784885ed4b37e21f7b09
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-09-14 11:50:57 +0200

    Correction

---
+++ include/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php session_start()?>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="Projet_hopital/index.php">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Projet_hopital/index.php">Home</a></li>
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Projet_hopital/vue/about.php">About</a></li>
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Projet_hopital/vue/products.php">Products</a></li>
+                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Projet_hopital/vue/store.php">Store</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</body>
+</html>
