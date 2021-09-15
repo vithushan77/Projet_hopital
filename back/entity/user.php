0ba2f0f2ae060226182f16642e762c18d1d5c121
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-09-15 13:31:01 +0200

    Ajout du constructeur

--- back/entity/user.php
+++ back/entity/user.php
@@ -9,6 +9,27 @@ class user {
   private $mdp;
   private $role;
 
+  /**
+ * constructeur de la classe user.
+ * @param array $array
+ */
+public function __construct($array)
+{
+    $this->hydrate($array);
+}
+/**
+ * @param array $donnees
+ */
+public function hydrate($donnees)
+{
+    foreach($donnees as $key => $value){
+        $method = 'set'.ucfirst($key);
+        if(method_exists($this,$method)){
+            $this->$method($value);
+        }
+    }
+}
+
   public function getId()
   {
     return $this->id;
