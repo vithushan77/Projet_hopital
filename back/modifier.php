6bc8cf461b6a93e50cd17927921604fe443be3ad
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-09-25 18:39:35 +0200

    Page de traitement pour la modification des informations du profil user

---
+++ back/modifier.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/Projet_hopital/back/entity/user.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/Projet_hopital/back/manager/manager.php');
+
+$modify = new user($_POST);
+$manager = new manager();
+$manager->modifierProfil();
+
+ ?>
