bdb7f89311fe786eb8404bfed691164c5b9fad87
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-10-01 15:38:33 +0200

    Petite modification

--- back/modifier.php
+++ back/modifier.php
@@ -6,5 +6,6 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/Projet_hopital/back/manager/manager.php
 $modify = new user($_POST);
 $manager = new manager();
 $manager->modifierProfil();
-
+echo '<body onLoad="alert(\'Informations enregistrées\')">';
+echo '<meta http-equiv="refresh" content="0;URL=/Projet_hopital/forms/moncompte.php">';
  ?>
