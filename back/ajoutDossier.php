1d573be843c935e28a232c8775d3b0f35cbd6366
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-11-30 08:20:44 +0100

    Correction en cours...

--- back/ajoutDossier.php
+++ back/ajoutDossier.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once($_SERVER['DOCUMENT_ROOT'].'/Projet_hopital/back/entity/user.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/Projet_hopital/back/entity/dossier-ad.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/Projet_hopital/back/manager/manager.php');
 
+$user = new user($_POST);
 $newDossier = new dossier($_POST);
 $manager = new manager();
-$manager->ajoutDossierAdmission($newDossier);
+$manager->ajoutDossierAdmission($user, $newDossier);
 
  ?>
